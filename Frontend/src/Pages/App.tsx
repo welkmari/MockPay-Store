efc13e21d8e2fc25872c01d5650c60f0780e850a
Maria Helena Welk <welktech46@gmail.com>
2025-12-03 15:11:50 -0300

    Update App.tsx

--- Frontend/src/Pages/App.tsx
+++ Frontend/src/Pages/App.tsx
@@ -2,12 +2,11 @@ import { Header } from '../Components/Header'
 import Banner from '../Assets/Banner.png'
 
 export const App: React.FC = () => {
-
   return (
     <>
       <Header/>
       <img src={Banner} alt="Banner"/>
-      <h2 className='text-3xl'>Novos Produtos</h2>
+      <h2 className='font-semibold'>Novos Produtos</h2>
       <p></p>
     </>
   );
